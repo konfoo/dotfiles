3466508d58eed6c68362eabbf9c26765047bf376
konfoo <ysk.kondo@gmail.com>
2013-08-09 00:28:08 +0900

    added antigen

---
+++ _zsh/antigen_env.zsh
@@ -0,0 +1,10 @@
+antigen use oh-my-zsh
+antigen bundle git
+
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# theme
+antigen theme candy
+
+antigen apply
--- _zsh/prompt.zsh
+++
@@ -1,6 +0,0 @@
-local GREEN=$'%{\e[1;32m%}'
-local YELLOW=$'%{\e[1;33m%}'
-local BLUE=$'%{\e[1;34m%}'
-local PURPLE=$'%{\e[0;35m%}'
-local DEFAULT=$'%{\e[m%}'
-PROMPT=$'\n'$PURPLE'${USER}@${HOSTNAME} %~ '$'\n'$DEFAULT'%(!.#.$) '
