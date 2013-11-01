208de0fb94ed68b58485aad1536a163baa84dd5b
konfoo <ysk.kondo@gmail.com>
2013-11-01 11:54:22 +0900

    antigen周り見直し

--- _zsh/_antigen_env.zsh
+++ _zsh/_antigen_env.zsh
@@ -1,5 +1,7 @@
 antigen use oh-my-zsh
 antigen bundle git
+antigen bundle rvm
+#antigen bundle tmux
 antigen bundle zsh-users/zsh-syntax-highlighting
 
 # theme
--- _zsh/alias.zsh
+++ _zsh/alias.zsh
@@ -1,33 +1,13 @@
-# PAGER
-if brew list vim > /dev/null  2>&1; then
-    alias vless=`find $(brew --cellar vim) -name less.sh | sort -r | head -1`
-    export PAGER=vless
-fi
-GIT_PAGER=$PAGER
-
 # z.sh
 if brew list z > /dev/null 2>&1; then
     _Z_CMD=j
     source `find $(brew --cellar z) -name z.sh | sort -r | head -1`
 fi
 
-# ls color
-if brew list coreutils > /dev/null 2>&1; then
-    alias ls="gls --color"
-else
-    alias ls="ls -G"
-fi
-
-# ls alias
-alias l='ls -F'
-alias la='ls -AF'
-alias ll='ls -lF'
-alias lla='ls -lAF'
-
 # command aliases
 alias vi="vim"
-alias pd='popd'
-alias zshload="source ~/.zshrc"
+alias zshload="source ~/.zshenv ~/.zshrc"
+[[ -s `which vimpage` ]] && export PAGER=vimpager
 [[ -s `which tmux` ]] && alias tmux='tmux -2'
 [[ -s `which htop` ]] && alias top='htop'
 
